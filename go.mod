9168d36f948d056f3fb719ada0a499f71bfa1f72
Ayooluwa Isaiah <ayisaiah@gmail.com>
2024-10-30 06:23:06 +0100

    starter code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/betterstack-community/otel-metrics-golang
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world!"))
+	})
+
+	log.Println("Starting HTTP server on port 8000")
+
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+}
